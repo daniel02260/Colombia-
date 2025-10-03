3ddb4ac7b34e870dc6fde8489b96dfc04321af9f
daniel02260 <nomelindaniel@gmail.com>
2025-10-03 19:45:41 +0000

    segunda parte2 corte

--- Colombia/src/comandos/home /index.jsx
+++ Colombia/src/comandos/home /index.jsx
@@ -1,7 +1,71 @@
-function Home(){
-    return(
-        <p>home</p>
-    );
+import React, { useEffect, useState } from "react";
 
+function Home() {
+  const [departamentos, setDepartamentos] = useState(null);
+  const [capitales, setCapitales] = useState(null);
+  const [modo, setModo] = useState("departamentos");
+  
+
+  useEffect(() => {
+    const urlDpt =
+      "https://gist.githubusercontent.com/diaztibata/fe3d238ee6b59ef71c8001654441a9f6/raw/4974a1b1cab3ac606dd96aa2d34d6e7c8e007daf/departamentosglobal.json";
+    const urlCpt =
+      "https://gist.githubusercontent.com/diaztibata/fe3d238ee6b59ef71c8001654441a9f6/raw/4974a1b1cab3ac606dd96aa2d34d6e7c8e007daf/capitalesglobal.json";
+
+    const fetchJson = async (url, setter) => {
+      try {
+        const resp = await fetch(url);
+        if (!resp.ok) throw new Error("Error al cargar JSON: " + resp.status);
+        const json = await resp.json();
+        setter(json);
+      } catch (error) {
+        console.error("Fetch error:", error);
+      }
+    };
+
+    fetchJson(urlDpt, setDepartamentos);
+    fetchJson(urlCpt, setCapitales);
+  }, []);
+let lista=[];
+if(modo==="capitales"&& capitales){
+    lista= capitales.data.cpt;
 }
-export default Home
+if(modo==="departamentos"&& departamentos){
+    lista= departamentos.data.dpt;
+ }
+ 
+  let listaFiltrada;
+
+  if (busqueda.length >= 2) {
+    listaFiltrada = lista.filter((unavuelta) =>
+      unavuelta.nm.toLowerCase().includes(busqueda.toLowerCase())
+    );
+  } else {
+    listaFiltrada = lista; // sin filtrar
+  }
+  return (
+    <>
+    <h3>{modo}</h3>
+      <input type="text" 
+      value={busqueda}
+          onChange={(e) => setBusqueda(e.target.value)}
+        />
+      <div className="Filtro">
+        <button>deptartamenrtos</button>
+        <button>capitales</button>
+      </div>
+         <ul className="Lista">
+        {listaFiltrada.length > 0 ? (
+          listaFiltrada.map((item) => (
+            <li key={item.id}>{item.nm}
+            </li>
+          ))
+        ) : (
+          <li>Cargando datos...</li>
+        )}
+      </ul>
+    </>
+  );
+}
+
+export default Home;
